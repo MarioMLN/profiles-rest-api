b979ebea723381f7892502131273238775b7aac9
MarioMLN <mariomlneto321@gmail.com>
2024-10-03 17:18:40 -0300

    Add HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 class HelloApiView(APIView):
     """Test API View"""
+
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
